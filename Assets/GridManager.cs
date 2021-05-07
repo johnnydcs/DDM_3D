6bc7e45ec1ad3c274c6c56940f13046021ac2d24
Johnny <johnnydcs@yahoo.com>
2021-05-06 19:29:53 -0700

    Initial commit; Basic grid and tile color changing
    13x19 grid of black tiles that may be rendered into red or blue.
    Next step: work on 2d raycasting to create dice dimension paths.

---
+++ Assets/GridManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public Sprite RedRef;
+    public Sprite BlueRef;
+    public Sprite BlackRef;
+
+    private readonly int Rows = 19;
+    private readonly int Cols = 13;
+
+    public GameObject[,] GridBoard;
+    
+    public GameObject TileRef;
+
+    // Start is called before the first frame Update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void GenerateGrid()
+    {
+        // Create all black tiles
+        GameObject[,] GridBoard = new GameObject[Rows, Cols];
+
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Cols; j++)
+            {
+                GridBoard[i, j] = Instantiate(TileRef, new Vector3(j, i, 0), Quaternion.identity);
+            }
+        }
+
+        // Make Red and Blue starting tiles - Player Avatar
+        GridBoard[0, 6].GetComponent<SpriteRenderer>().sprite = RedRef;
+
+        GridBoard[18, 6].GetComponent<SpriteRenderer>().sprite = BlueRef;
+    }
+
+    void CheckDimension()
+    {
+
+    }
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Sprite RedRef;
+    public Sprite BlueRef;
+    public Sprite BlackRef;
+
+    public SpriteRenderer spriteRenderer;
+    
+    // Awake is called before Start
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = BlackRef;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
